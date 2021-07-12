8c590c329151dfc2cf7dad4b9021a428ac794fe4
Guilherme Pereira de Oliveira <guilhermepdo1508@gmail.com>
2021-07-11 22:51:08 -0300

    init

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <h1>Meus projetos:</h1>
       <ul>
-        <a href="http://www.guidpo0.github.io/trybewallet" rel="noreferrer" target="_blank">Trybewallet</a>
+        <a href="http://guidpo0.github.io/trybewallet" rel="noreferrer" target="_blank">Trybewallet</a>
       </ul>
     </div>
   );

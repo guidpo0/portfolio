b86222d787da103d95252720b1f33d81fc6886ac
Gui Pereira <guilhermepdo1508@gmail.com>
2021-08-16 22:25:21 -0300

    Feita pagina de sobre

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,8 +1,32 @@
 import React, { useEffect } from 'react';
 import './About.css';
 import aboutPicture from '../images/aboutPicture.jpg';
+import skillCss from '../images/skillCss.png';
+import skillGit from '../images/skillGit.png';
+import skillHtml from '../images/skillHtml.png';
+import skillJs from '../images/skillJs.png';
+import skillReact from '../images/skillReact.png';
+import skillRedux from '../images/skillRedux.png';
 
 function About() {
+  const softSkills = [
+    'Inteligência Emocional',
+    'Empatia',
+    'Escuta Ativa',
+    'Assertividade',
+    'Colaboração',
+    'Comunicação',
+    'Pensamento Crítico',
+  ];
+  const hardSkills = [
+    skillCss,
+    skillGit,
+    skillHtml,
+    skillJs,
+    skillReact,
+    skillRedux,
+  ];
+
   useEffect(() => {
     const navBarOpened = document.querySelector('.navbar-opened');
     const aboutPage = document.querySelector('.about-page-container');
@@ -20,9 +44,30 @@ function About() {
         <h2>Sou o Guilherme</h2>
         <h3>Paulista, de 96, apaixonado por viagens e aspirante a jogador de basquete.</h3>
         <h4>Tenho como pilar minha família e carrego como valores a honestidade e o respeito ao próximo.</h4>
-        <h6>Minha jornada em tecnologia se iniciou em 2021 com o descontentamento com os métodos de ensino tradicionais, pelos quais estava buscando ingressar no mesmo mercado.</h6>
-        <h6>Foi quando decidi me aventurar com métodos pouco experimentados no Brasil e iniciei minha jornada rumo ao conhecimento em desenvolvimento web full stack.</h6>
-        <h6>No momento já conclui o módulo de Front End na Trybe, empresa que escolhi para me apontar o caminho nessa jornada, e hoje estou estudando o Back End.</h6>
+        <h5>Minha jornada em tecnologia se iniciou em 2021 com o descontentamento com os métodos de ensino tradicionais, pelos quais estava buscando ingressar no mesmo mercado.</h5>
+        <h5>Foi quando decidi me aventurar com métodos pouco experimentados no Brasil e iniciei minha jornada rumo ao conhecimento em desenvolvimento web full stack.</h5>
+        <h5>No momento já conclui o módulo de Front End na Trybe, empresa que escolhi para me apontar o caminho nessa jornada, e hoje estou estudando o Back End.</h5>
+        <div>
+          <h2 >Habilidades</h2>
+          <h3>Soft Skills</h3>
+          <ul>
+            { softSkills.map((skill) => <li>{ skill }</li>)}
+          </ul>
+          <h3>Hard Skills</h3>
+          <div className="hard-skills">
+          {
+            hardSkills.map((skill, index) => (
+              <div>
+                <img
+                  src={ skill }
+                  alt="hard-skills logo"
+                  className={ `hard-skill-${index} hard-skill` }
+                />
+              </div>
+            ))
+          }            
+          </div>
+        </div>
       </main>
     </div>
   );

8e9615de7c9e6506662d95fdc0d3b7643e0fa4d6
Gui Pereira <guilhermepdo1508@gmail.com>
2021-09-02 18:44:45 -0300

    Adicionado projeto pixel arte

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -4,6 +4,7 @@ import Footer from '../components/Footer';
 import './Projects.css';
 import receitas from '../images/projects/receitas.png';
 import planetas from '../images/projects/planetas-starwars.jpeg';
+import pixel from '../images/projects/pixel.jpeg';
 
 function Projects() {
   return (
@@ -45,6 +46,25 @@ function Projects() {
             <div className="frontend-description">
               <p>
                 Essa aplicação dá ao usuário acesso a uma lista de planetas com a opção de realizar filtros nesta lista para procurar por planetas específicos.
+                <br />
+                <br />
+                Estilizado para mobile e desktop
+              </p>
+            </div>
+          </a>
+          <a
+            href="https://guidpo0.github.io/arte-em-pixels/"
+            rel="noreferrer"
+            target="_blank"
+            className="frontend-project"
+          >
+            <div className="frontend-image-container">
+              <img src={pixel} alt="prototipo projeto planetas starwars" />
+            </div>
+            <h5>Arte em Pixels</h5>
+            <div className="frontend-description">
+              <p>
+                Esta aplicação busca permitir ao usuário trabalhar sua criatividade por meio de uma paleta de cores.
                 <br />
                 <br />
                 Estilizado para mobile e desktop

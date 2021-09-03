eb3d7dd56cb131c0002370691f837462425bf9cf
Gui Pereira <guilhermepdo1508@gmail.com>
2021-09-03 12:36:26 -0300

    Adicionado projeto carta misteriosa

---
+++ src/helpers/projectsInfo.js
@@ -0,0 +1,50 @@
+import receitas from '../images/projects/receitas.png';
+import planetas from '../images/projects/planetas-starwars.jpeg';
+import pixel from '../images/projects/pixel.jpeg';
+import carta from '../images/projects/carta.jpeg';
+import todo from '../images/projects/todo.jpeg';
+
+const projectsInfo = [
+  {
+    link: 'https://guidpo0.github.io/receitas',
+    thumb: receitas,
+    alt: 'prototipo projeto app de receitas',
+    title: 'App de Receitas',
+    description: 'App de receitas onde é possível ver, buscar, filtrar, favoritar e acompanhar o processo de preparação de receitas e drinks!',
+    style: 'Estilizado para mobile e desktop',
+  },
+  {
+    link: 'https://guidpo0.github.io/planetas-starwars/',
+    thumb: planetas,
+    alt: 'prototipo projeto planetas starwars',
+    title: 'Planetas Starwars',
+    description: 'Essa aplicação dá ao usuário acesso a uma lista de planetas com a opção de realizar filtros nesta lista para procurar por planetas específicos.',
+    style: 'Estilizado para mobile e desktop',
+  },
+  {
+    link: 'https://guidpo0.github.io/carta-misteriosa/',
+    thumb: carta,
+    alt: 'prototipo projeto carta misteriosa',
+    title: 'Carta Misteriosa',
+    description: 'Esta aplicação permite ao usuário codificar uma frase digitada.',
+    style: 'Estilizado para mobile e desktop',
+  },
+  {
+    link: 'https://guidpo0.github.io/lista-de-tarefas/',
+    thumb: todo,
+    alt: 'prototipo projeto lista de tarefas',
+    title: 'Lista de Tarefas',
+    description: 'Esta aplicação permite ao usuário anotar tarefas e manipulá-las.',
+    style: 'Estilizado para mobile e desktop',
+  },
+  {
+    link: 'https://guidpo0.github.io/arte-em-pixels/',
+    thumb: pixel,
+    alt: 'prototipo projeto arte em pixels',
+    title: 'Arte em Pixels',
+    description: 'Esta aplicação busca permitir ao usuário trabalhar sua criatividade por meio de uma paleta de cores.',
+    style: 'Estilizado para mobile e desktop',
+  },
+];
+
+export default projectsInfo;
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -2,10 +2,7 @@ import React from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import './Projects.css';
-import receitas from '../images/projects/receitas.png';
-import planetas from '../images/projects/planetas-starwars.jpeg';
-import pixel from '../images/projects/pixel.jpeg';
-import todo from '../images/projects/todo.jpeg';
+import projectsInfo from '../helpers/projectsInfo';
 
 function Projects() {
   return (
@@ -15,82 +12,29 @@ function Projects() {
         <h1>Meus projetos</h1>
         <h3>Projetos em Front-End:</h3>
         <div className="projects">
-          <a
-            href="https://guidpo0.github.io/receitas"
-            rel="noreferrer"
-            target="_blank"
-            className="frontend-project"
-          >
-            <div className="frontend-image-container">
-              <img src={receitas} alt="prototipo projeto app de receitas" />
-            </div>
-            <h5>App de Receitas</h5>
-            <div className="frontend-description">
-              <p>
-                App de receitas onde é possível ver, buscar, filtrar, favoritar e acompanhar o processo de preparação de receitas e drinks!
-                <br />
-                <br />
-                Estilizado para mobile e desktop
-              </p>
-            </div>
-          </a>
-          <a
-            href="https://guidpo0.github.io/planetas-starwars/"
-            rel="noreferrer"
-            target="_blank"
-            className="frontend-project"
-          >
-            <div className="frontend-image-container">
-              <img src={planetas} alt="prototipo projeto planetas starwars" />
-            </div>
-            <h5>Planetas Starwars</h5>
-            <div className="frontend-description">
-              <p>
-                Essa aplicação dá ao usuário acesso a uma lista de planetas com a opção de realizar filtros nesta lista para procurar por planetas específicos.
-                <br />
-                <br />
-                Estilizado para mobile e desktop
-              </p>
-            </div>
-          </a>
-          <a
-            href="https://guidpo0.github.io/lista-de-tarefas/"
-            rel="noreferrer"
-            target="_blank"
-            className="frontend-project"
-          >
-            <div className="frontend-image-container">
-              <img src={todo} alt="prototipo projeto planetas starwars" />
-            </div>
-            <h5>Lista de Tarefas</h5>
-            <div className="frontend-description">
-              <p>
-                Esta aplicação permite ao usuário anotar tarefas e manipulá-las.
-                <br />
-                <br />
-                Estilizado para mobile e desktop
-              </p>
-            </div>
-          </a>
-          <a
-            href="https://guidpo0.github.io/arte-em-pixels/"
-            rel="noreferrer"
-            target="_blank"
-            className="frontend-project"
-          >
-            <div className="frontend-image-container">
-              <img src={pixel} alt="prototipo projeto planetas starwars" />
-            </div>
-            <h5>Arte em Pixels</h5>
-            <div className="frontend-description">
-              <p>
-                Esta aplicação busca permitir ao usuário trabalhar sua criatividade por meio de uma paleta de cores.
-                <br />
-                <br />
-                Estilizado para mobile e desktop
-              </p>
-            </div>
-          </a>
+          {
+            projectsInfo.map(({ link, thumb, alt, title, description, style }) => (
+              <a
+                href={ link }
+                rel="noreferrer"
+                target="_blank"
+                className="frontend-project"
+              >
+                <div className="frontend-image-container">
+                  <img src={ thumb } alt={ alt } />
+                </div>
+                <h5>{ title }</h5>
+                <div className="frontend-description">
+                  <p>
+                    { description }
+                    <br />
+                    <br />
+                    { style }
+                  </p>
+                </div>
+              </a>
+            ))
+          }
         </div>     
       </main>
       <Footer />

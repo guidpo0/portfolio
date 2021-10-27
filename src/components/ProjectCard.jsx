722312ddbae79ab6df465b4d50d70abd4dbe98cb
Gui Pereira <guilhermepdo1508@gmail.com>
2021-10-27 17:01:40 -0300

    Estilizado project card

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,21 +1,37 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './ProjectCard.css';
+import repoIcon from '../images/repoIcon.png';
 
 function ProjectCard({ project }) {
   const {
     thumb, link, title, area, stacks,
   } = project;
   return (
-    <div style={{ backgroundImage: `url(${thumb})` }}>
-      <div>
-        <h5>{title}</h5>
-        <a href={link}>Repo</a>
+    <div
+      className="project-card-container"
+      style={{
+        background: `url(${thumb})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+      }}
+    >
+      <div className="project-card-title-container">
+        <h3>{title}</h3>
+        <a href={link} target="_blank" rel="noreferrer">
+          <img width="20" src={repoIcon} alt="Repo icon" />
+          Repo
+        </a>
       </div>
-      <div>
-        <h6>{area}</h6>
-        <h6>Stacks:</h6>
+      <div className="project-card-stacks-container">
+        <h4>
+          Projeto
+          {' '}
+          {area}
+        </h4>
+        <h5>Stacks:</h5>
         <div>
-          { stacks.map((stack) => (<p>{stack}</p>))}
+          { stacks.map((stack) => (<h6>{stack}</h6>))}
         </div>
       </div>
     </div>
